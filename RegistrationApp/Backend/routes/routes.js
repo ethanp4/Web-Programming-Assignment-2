98c2f824bc523060a2b535905a234de2dce716ac
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-04-02 16:23:45 -0600

    add route for /

--- RegistrationApp/Backend/routes/routes.js
+++ RegistrationApp/Backend/routes/routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { submitForm } = require('../controllers/controller')
 
+router.get('/', (req, res) => {
+  res.redirect('./Frontend/index.html')
+})
+
 router.post('/submitForm', submitForm)
 
 module.exports = router
