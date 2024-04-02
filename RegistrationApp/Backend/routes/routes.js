f82ca60fdedcf3ee75a6390e4ed80df163df0bc8
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-04-02 16:26:59 -0600

    remove / route

--- RegistrationApp/Backend/routes/routes.js
+++ RegistrationApp/Backend/routes/routes.js
@@ -2,10 +2,6 @@ const express = require('express')
 const router = express.Router()
 const { submitForm } = require('../controllers/controller')
 
-router.get('/', (req, res) => {
-  res.redirect('./Frontend/index.html')
-})
-
 router.post('/submitForm', submitForm)
 
 module.exports = router
