d9e44b9e50c1be25625869b651bb05cb109a370f
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-04-01 18:06:12 -0600

    change to a different weather source

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,91 +1,30 @@
 $(() => {
-  // if the browser supports geolocation 
-  if (navigator.geolocation) {
-    //get the location and send it to callback function
-    navigator.geolocation.getCurrentPosition(getCoordsFromGeolocation)
-  }
+  url = "https://wttr.in/?format=j1"
+  $.getJSON(url, (weather) => {
+    console.log(weather)
+    updatePage(weather)
+  })
 
-  //organise coords and then get the weather
-  function getCoordsFromGeolocation(location) {
-    let latitude = location.coords.latitude
-    let longitude = location.coords.longitude
-
-    console.log(location)
-
-    getWeather(latitude, longitude)
-  }
-
-  // getWeather(0, 0)
-
-  //if geolocation wasnt used
-  function getCoordsFromUser() {
-
-    getWeather(latitude, longitude)
-  }
-
-  //finally fetch the url here
-  function getWeather(latitude, longitude) {
-    url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto&forecast_days=1`
-    $.getJSON(url, (weather) => {
-      console.log(weather)
-      updatePage(weather, location)
-    })
-  }
 
   //add the data to the page
   function updatePage(weather) {
-    outlook = lookupWeatherCode(weather.current.weather_code)
+    let outlook = weather.current_condition[0].weatherDesc[0].value
+    let city = weather.nearest_area[0].areaName[0].value
+    let current = weather.current_condition[0].temp_C
+    let high = weather.weather[0].maxtempC
+    let low = weather.weather[0].mintempC
+    let sunrise = weather.weather[0].astronomy[0].sunrise
+    let sunset = weather.weather[0].astronomy[0].sunset
 
     $("#nogeo").hide()
     $("#header").show()
     $("#content").show()
 
-    $("#header").text(`It's ${weather.current.temperature_2m}${weather.current_units.temperature_2m} and ${outlook}`)
-    $("#high").text(`${weather.daily.temperature_2m_max[0]}${weather.current_units.temperature_2m}`)
-    $("#low").text(`${weather.daily.temperature_2m_min[0]}${weather.current_units.temperature_2m}`)
+    $("#header").text(`It's ${current}°C and ${outlook.toLowerCase()} in ${city}`)
+    $("#high").text(`${high}°C`)
+    $("#low").text(`${low}°C`)
 
-    $("#sunrise").text(`${weather.daily.sunrise}`.substring(11, 16))
-    $("#sunset").text(`${weather.daily.sunset}`.substring(11, 16))
-  }
-
-  //convert wmo weather code to string https://open-meteo.com/en/docs/#weathervariables
-  function lookupWeatherCode(code) {
-    switch (code) {
-      case 0:
-        return "clear";
-      case 1:
-      case 2:
-      case 3:
-        return "partly cloudy";
-      case 45:
-      case 48:
-        return "foggy";
-      case 51:
-      case 53:
-      case 55:
-      case 61:
-      case 63:
-      case 65:
-      case 80:
-      case 81:
-      case 82:
-        return "raining";
-      case 56:
-      case 57:
-      case 66:
-      case 67:
-        return "sleeting";
-      case 71:
-      case 73:
-      case 75:
-      case 77:
-      case 85:
-      case 86:
-        return "snowing";
-      case 95:
-      case 96:
-      case 99:
-        return "storming";
-    }
+    $("#sunrise").text(sunrise)
+    $("#sunset").text(sunset)
   }
 })
